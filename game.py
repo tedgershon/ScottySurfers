c33169cd9b424c9dcf44dfa4e66c0ab6aca89f89
dsblack0 <samhithad.1025@gmail.com>
2023-11-04 20:45:10 -0400

    fixed jonny's interesting mistakes

--- game.py
+++ game.py
@@ -7,15 +7,15 @@ def onAppStart(app):
     app.x = 0
     app.y = 0
     app.width = 600
-    app.height = 800
-    app.posX, app.posY = app.width/2, app.height/2
+    app.height = 700
     app.laneLength = app.width/3
-    app.restartGame(app)
+    restartGame(app)
     app.stepsPerSecond = 30
 
 def restartGame(app):
     app.instructions = True
     app.paused = True
+    app.posX, app.posY = app.width/2, app.height-50
 
 def onStep(app):
     if not app.paused:
@@ -30,16 +30,22 @@ def moveTA(app, drow):
 
 
 def onKeyPress(app, key):
-    if key == 's' and not app.paused:
-        takeStep(app)
-    elif key == 'p':
+    if key == 'p':
         app.paused = not app.paused
+    elif key == 'h':
+        app.instructions = not app.instructions
+    elif key == 'q':
+        app.gameOver = True
+    elif key == 'r':
+        restartGame(app)
+    
         
 def onKeyRelease(app, key):
     pass
 
 def onKeyHold(app, key):
-    pass
+    if 'left' in key:
+        app.posX -= 5
 
 def onMousePress(app, mouseX, mouseY):
     pass
@@ -56,21 +62,13 @@ def onMouseMove(app, mouseX, mouseY):
 def onResize(app):
     pass
 
-### View
-def buttons(xpos,ypos,colour,text,width,height):
-    drawRect(xpos, ypos, width, height, fill = colour)
-    drawLabel(msg, xpos+25, ypos+12, font = text)
-
-def gameOver():
-    while gameOver == False:
-
 def drawMainChar(app):
-    pass
+    drawCircle(app.posX, app.posY, 30)
 
 def randomNum(low, high):
     return random.randint(low, high)
 
-def showInstructions(app):
+def drawInstructions(app):
     if app.instructions == True:
         drawLabel("INSTRUCTIONS", 200, 50, size = 50, bold = True, fill = 'white')
         drawLabel('If the left arrow is pressed, it moves the piece to left', 200,100,size=14, bold = True, fill = 'white')
@@ -80,19 +78,15 @@ def showInstructions(app):
         drawLabel('If the e key is pressed, it rotates the piece counter-clockwise', 200,220,size=14, bold = True, fill = 'white')
         drawLabel('If the s key is pressed, it takes a step in the game', 200,250,size=14, bold = True, fill = 'white')
         drawLabel('Press h to leave instructions and continue game', 200,340, size = 14, bold = True, fill = 'cyan')
-    else:
-        pass
 
 def gameOver(app):
     if app.gameOver == True:
         drawLabel('GAME OVER', 200, 200, size = 50, bold = True, fill = 'silver', rotateAngle = 40, border = 'black')
         drawLabel('Press "r" to restart game. Max score is currently: ' + str(app.maxScore), 200, 30, size=14, bold = True, fill = 'purple')
-        drawLabel('HIGH SCORES', 75,200, size = 18, bold = True, fill='silver')
-        for i in range(len(app.highScores)):
-            drawLabel(str(app.highScores[i]), 50,220+i*20, size = 14, bold = True, fill='silver')
 
 def redrawAll(app):
     drawMainChar(app)
+    drawInstructions(app)
 
 ### Main
 def main():
