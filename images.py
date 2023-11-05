a6473270c867833b3021aa5737d0bb70c1fb1dc7
Ted Gershon <98845943+tedgershon@users.noreply.github.com>
2023-11-05 09:59:48 -0500

    Update images.py

--- images.py
+++ images.py
@@ -1,20 +1,12 @@
-from cmu_graphics import *
 from PIL import Image
 
 # this file contains all images used in background, obstacles, or characters
 # images formatted as jpg and referenced later in "TAList" in game.py
 
 # Backgrounds
-'''
-road = pygame.image.load("pictures/road.png")
-road = pygame.transform.scale(road,(400,800))
-grass = pygame.image.load("pictures/grass.jpg")
-grass = pygame.transform.scale(grass,(275,800))
-#Player sprite
-bike = pygame.image.load("pictures/bike1.png")
-bike = pygame.transform.scale(bike,(70,140))
-#obstacles
-'''
+
+# Characters
+
 # Obstacles
     #instructors
 inst1 = Image.open('images/inst1.jpg')
@@ -31,25 +23,25 @@ taH3 = CMUImage(taH3)
     #TAs
 ta1 = Image.open('images/ta1.jpg')
 ta1 = CMUImage(ta1)
-ta2 = Image.open('images/ta1.jpg')
+ta2 = Image.open('images/ta2.jpg')
 ta2 = CMUImage(ta2)
-ta3 = Image.open('images/ta1.jpg')
+ta3 = Image.open('images/ta3.jpg')
 ta3 = CMUImage(ta3)
-ta4 = Image.open('images/ta1.jpg')
+ta4 = Image.open('images/ta4.jpg')
 ta4 = CMUImage(ta4)
-ta5 = Image.open('images/ta1.jpg')
+ta5 = Image.open('images/ta5.jpg')
 ta5 = CMUImage(ta5)
-ta6 = Image.open('images/ta1.jpg')
+ta6 = Image.open('images/ta6.jpg')
 ta6 = CMUImage(ta6)
-ta7 = Image.open('images/ta1.jpg')
+ta7 = Image.open('images/ta7.jpg')
 ta7 = CMUImage(ta7)
-ta8 = Image.open('images/ta1.jpg')
+ta8 = Image.open('images/ta8.jpg')
 ta8 = CMUImage(ta8)
-ta9 = Image.open('images/ta1.jpg')
+ta9 = Image.open('images/ta9.jpg')
 ta9 = CMUImage(ta9)
-ta10 = Image.open('images/ta1.jpg')
+ta10 = Image.open('images/ta10.jpg')
 ta10 = CMUImage(ta10)
-ta11 = Image.open('images/ta1.jpg')
+ta11 = Image.open('images/ta11.jpg')
 ta11 = CMUImage(ta11)
-ta12 = Image.open('images/ta1.jpg')
+ta12 = Image.open('images/ta12.jpg')
 ta12 = CMUImage(ta12)
